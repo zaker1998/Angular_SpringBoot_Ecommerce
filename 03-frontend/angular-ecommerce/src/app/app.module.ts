9bc4412e041fc36f6e9269c0f4cf0227c59ea5fe
Marat Dussaliyev <a01642049@unet.univie.ac.at>
2025-02-03 17:42:12 +0100

    import of HttpClients + Product Service init with injection

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -4,7 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { HttpClientModule } from '@angular/common/http';
+//import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { ProductService } from './services/product.service';
 
 @NgModule({
@@ -15,9 +16,9 @@ import { ProductService } from './services/product.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    //HttpClientModule
   ],
-  providers: [ProductService],
+  providers: [ProductService, provideHttpClient()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
