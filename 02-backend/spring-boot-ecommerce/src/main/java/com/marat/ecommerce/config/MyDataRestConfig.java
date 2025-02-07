ea5decb503b7493e79b953cf301dafa3f3eec599
Marat Dussaliyev <a01642049@unet.univie.ac.at>
2025-02-07 17:08:42 +0100

    exposing of ids in Spring configuration

--- 02-backend/spring-boot-ecommerce/src/main/java/com/marat/ecommerce/config/MyDataRestConfig.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/marat/ecommerce/config/MyDataRestConfig.java
@@ -2,13 +2,19 @@ package com.marat.ecommerce.config;
 
 import com.marat.ecommerce.entity.Product;
 import com.marat.ecommerce.entity.ProductCategory;
+import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
@@ -42,5 +48,17 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
     }
 
     private void exposeIds(RepositoryRestConfiguration config) {
+        //get a collection of all entity classes from entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        List<Class> entityClasses = new ArrayList<>();
+
+        for(EntityType entityType : entities) {
+            entityClasses.add(entityType.getJavaType());
+        }
+
+        //expose the entity ids for the array of entity/domain types
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 }
